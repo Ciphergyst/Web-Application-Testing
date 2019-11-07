30c17129866325bfd66db64f85b4660ff54b4379
ciphergyst <ciphergyst@vivaldi.net>
2019-11-06 22:01:49 -0800

    added background and scoreboard container

---
+++ batter-up/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Scoreboard goes here!</p>
+      </header>
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
